b530b509464a7ff041470c2ab722f885fd109db1
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-05-22 15:21:05 +0530

    Removed App in apps

--- apps/web/components/Sample.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ReuseLoginForm } from "@shared-test/reusejsv2-sample-login-form";
-
-const Sample = () => {
-  return <ReuseLoginForm />;
-};
-
-export default Sample;
--- apps/web/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./pages/**/*.{ts,tsx}", "../../packages/**/*.{js,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
