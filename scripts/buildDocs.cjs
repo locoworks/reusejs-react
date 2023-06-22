c0bb9cc258ec2b2128fb66f639258a0bcb1a2f93
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-06-22 14:49:58 +0530

    Case change for build scripts

--- scripts/buildDocs.cjs
+++ scripts/buildDocs.cjs
@@ -1,6 +1,6 @@
 const updatePackages = require("./updatePackages.cjs");
 const copyComponentsNPages = require("./copyComponentsNPages.cjs");
-const genTailwind = require("./gentailwind.cjs");
+const genTailwind = require("./genTailwind.cjs");
 
 updatePackages();
 copyComponentsNPages();
