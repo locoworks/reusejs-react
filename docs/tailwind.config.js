25ae12c6cc14e47b3d0ab8a648055c2a779292d4
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-06-22 13:45:02 +0530

    Added babel-loader to docs next config

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,25 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../components/button/**/*.{js,jsx.ts.tsx,mdx}",
-    "../../components/code-preview/**/*.{js,jsx.ts.tsx,mdx}",
-    "../../components/input/**/*.{js,jsx.ts.tsx,mdx}",
-    "../../components/input-group/**/*.{js,jsx.ts.tsx,mdx}",
-    "../../components/otp-input/**/*.{js,jsx.ts.tsx,mdx}",
-    "../node_modules/@locoworks/**/*.{js,jsx.ts.tsx,mdx}",
-    "../**/*/reusejs-react-code-preview/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+"./components/**/*.{js,ts,jsx,tsx,mdx}",
+"./app/**/*.{js,ts,jsx,tsx,mdx}",
+"../../components/button/**/*.{js,jsx.ts.tsx,mdx}",
+"../../components/code-preview/**/*.{js,jsx.ts.tsx,mdx}",
+"../../components/input/**/*.{js,jsx.ts.tsx,mdx}",
+"../../components/input-group/**/*.{js,jsx.ts.tsx,mdx}",
+"../../components/otp-input/**/*.{js,jsx.ts.tsx,mdx}",
+"../node_modules/@locoworks/**/*.{js,jsx.ts.tsx,mdx}",
+"../**/*/reusejs-react-code-preview/**/*.{js,ts,jsx,tsx,mdx}"
+],
+  theme: {"extend":{"backgroundImage":{"gradient-radial":"radial-gradient(var(--tw-gradient-stops))","gradient-conic":"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"}}},
+plugins: [require("@tailwindcss/typography"),require("@tailwindcss/forms")],
 };
