cbf8265bb18ebe0dcfdaaee2710003c6d4f62cd5
Betalectic <betalectic@Betalectics-MacBook-Air.local>
2023-06-27 19:03:20 +0530

    input forwardref added

---
+++ development/input-app/components/input/Forwardrefinput/Forwardrefinput.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect,Ref, InputHTMLAttributes } from "react";
+import { ReuseInput } from "@locoworks/reusejs-react-input";
+import { useRef } from "react";
+import { HeadlessButton } from "@locoworks/reusejs-react-button";
+const Forwardrefinput = () => {
+    const sampleref = useRef<HTMLInputElement>(null);    
+
+       useEffect(() => {
+      if(sampleref.current){
+        sampleref.current.focus();
+      }
+     }, []);
+    const handleClick = () => {
+        // Access the input value using the ref
+        if(sampleref.current){
+
+            console.log(">>>",sampleref.current,sampleref.current.value)
+            // alert("The Value of the input is " , sampleref.current)
+            let a = sampleref.current.value;
+            alert(`${a} is the value of input `)
+        }
+
+      };
+  return (
+    <div className="flex gap-x-3 justify-center py-10 mt-10 border rounded bg-gray-50">
+      <ReuseInput inputRef = {sampleref}  placeholder="start typing." />
+      <HeadlessButton className="bg-blue-200 border border-blue-400 rounded px-3 py-1" onClick={handleClick}>
+        Get Value
+      </HeadlessButton>
+      
+    </div>
+  );
+};
+
+export default Forwardrefinput;
