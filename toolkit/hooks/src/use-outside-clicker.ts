7a073f8febf5a832cfc299711c320b237c868923
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-06-28 11:55:56 +0530

    Added docs for useOutsideClickerHook

--- toolkit/hooks/src/use-outside-clicker.ts
+++ toolkit/hooks/src/use-outside-clicker.ts
@@ -1,3 +1,11 @@
+/**
+ * useOutsideClicker
+ *
+ * A custom hook that detects clicks outside a specified element and invokes a callback function.
+ *
+ * @param {Function} callback - The callback function to be invoked on outside clicks.
+ * @returns {React.MutableRefObject} - A mutable ref object that should be assigned to the component or element to track outside clicks.
+ */
 import React from "react";
 
 export let useOutsideClicker = (callback: () => void) => {
