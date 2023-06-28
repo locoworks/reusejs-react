e199aa31f0e9ee0e3025f94871c8ca5cadf77fe6
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-06-28 14:59:29 +0530

    Copy pages configured

--- scripts/copyComponentsNPages.mjs
+++ scripts/copyComponentsNPages.mjs
@@ -9,16 +9,38 @@ const devAppsPath = "./development";
 const getComponentFoldersList = () => {
   const componentList = fs
     .readdirSync(devAppsPath)
+    .filter((ele) => !ele.includes("toolkits-app"))
     .map((app) => "./development/" + app + "/components");
   const pagesList = fs
     .readdirSync(devAppsPath)
+    .filter((ele) => !ele.includes("toolkits-app"))
     .map((app) => "./development/" + app + "/pages");
   componentList.forEach((sourcePath) =>
     copyToFolder(sourcePath, "./docs/components")
   );
   pagesList.forEach((sourcePath) => copyToFolder(sourcePath, "./docs/pages"));
+  copyToolkits("./development/toolkits-app/pages", "./docs/pages");
 };
 
+const copyToolkits = (source, target) => {
+  const filterFn = (source) => {
+    if (
+      source.includes("_app.tsx") ||
+      source.includes("_document.tsx") ||
+      source.includes("index.tsx")
+    )
+      return false;
+    return true;
+  };
+
+  ncp(source, target, { filter: filterFn }, function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      // console.log("Component Folder created successfully.");
+    }
+  });
+};
 const copyToFolder = (source, target) => {
   const filterFn = (source) => {
     if (
