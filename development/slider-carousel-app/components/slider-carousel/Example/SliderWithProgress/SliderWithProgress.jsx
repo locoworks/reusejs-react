99d4a1e0eb01b7dd69e7af8d0b78e45a5c8f0055
Md Amir Sohail <betalectic@Betalectics-MacBook-Air.local>
2023-07-11 14:27:40 +0530

    updated map

--- development/slider-carousel-app/components/slider-carousel/Example/SliderWithProgress/SliderWithProgress.jsx
+++ development/slider-carousel-app/components/slider-carousel/Example/SliderWithProgress/SliderWithProgress.jsx
@@ -44,22 +44,23 @@ const Slide = ({ heading, phrase }) => {
     </div>
   );
 };
-const slides = [];
-List.forEach((element, index) => [
-  slides.push(
+
+const slidesArray = List.map((element, index) => {
+  return (
     <Slide
       heading={element.heading}
       phrase={element.phrase}
-      key={"slide" + index}
+      key={"Slide" + index}
     />
-  ),
-]);
+  );
+});
+
 const SliderWithProgress = () => {
   return (
     <div className="flex flex-col items-center gap-x-3 justify-center py-10 mt-10 border rounded bg-gray-50 px-6">
       <Slider
         slideInterval={2000}
-        statusArray={slides}
+        statusArray={slidesArray}
         loop={true}
         enableButton={true}
         progressStyle="flex h-full rounded-full bg-green-500"
