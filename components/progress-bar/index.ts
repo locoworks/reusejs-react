ff2b53348a965ce3e1eda3b4aff7aa45fea7af03
Md Amir Sohail <betalectic@Betalectics-MacBook-Air.local>
2023-07-13 11:03:06 +0530

    updated export for progress Component

--- components/progress-bar/index.ts
+++ components/progress-bar/index.ts
@@ -1,3 +1,2 @@
 export {default as ProgressBar} from "./src/ProgressBar"
-export {default as HalfCircularProgress} from "./src/HalfCircularProgress"
 export {default as CircularProgressBar} from "./src/CircularProgressBar"
