c3c5dd4999006aa61d14079f3114fba5e04075db
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-07-13 10:38:24 +0530

    Script optimisation for toc generation

--- scripts/createTocJSON.mjs
+++ scripts/createTocJSON.mjs
@@ -3,54 +3,11 @@ import markdown from "remark-parse";
 import extractToc from "remark-extract-toc";
 import fs from "node:fs";
 
-const createTocJSON = () => {
-  const path = "./development";
-  const dir = fs
-    .readdirSync(path)
-    .filter((ele) => !ele.includes("toolkits-app"));
-  // console.log(">>", path);
-  dir.map((ele) => {
-    ele = ele.split("-");
-    ele.pop();
-    ele = ele.join("-");
-    // path + "/" + ele + "-app/pages/" + ele + "/index.mdx";
-    const data = exportToC(
-      path + "/" + ele + "-app/pages/" + ele + "/index.mdx"
-    );
-    const jsonData = JSON.stringify(data, null, 2);
-    fs.writeFile("./docs/public/" + ele + "-toc.json", jsonData, (err) => {
-      if (err) {
-        console.error("Error writing JSON file:", err);
-      } else {
-        // console.log("JSON file created successfully!");
-      }
-    });
-  });
-  const toolkitsPaths = [
-    path + "/toolkits-app/pages/hooks",
-    path + "/toolkits-app/pages/utils",
-  ];
-  // console.log(">>>>", toolkitsPaths);
-  toolkitsPaths.forEach((file) => {
-    const list = fs.readdirSync(file).filter((ele) => ele.includes("mdx"));
-    // console.log("List in ", file, ">>>>", list);
-    list.forEach((ele) => {
-      const path = file + "/" + ele;
-      // console.log(">>>>", path);
-      const data = exportToC(path);
-      const jsonData = JSON.stringify(data, null, 2);
-      fs.writeFile(
-        "./docs/public/" + ele.replace(".mdx", "") + "-toc.json",
-        jsonData,
-        (err) => {
-          if (err) {
-            console.error("Error writing JSON file:", err);
-          } else {
-            // console.log("JSON file created successfully!");
-          }
-        }
-      );
-    });
+const writeToFile = (path, data) => {
+  fs.writeFile(path, data, (err) => {
+    if (err) {
+      console.log("Error while writing to path>>", path, err);
+    }
   });
 };
 
@@ -64,5 +21,34 @@ const exportToC = (path) => {
   return tree;
 };
 
+const createTocJSON = () => {
+  const path = "./development";
+
+  const dir = fs
+    .readdirSync(path)
+    .filter((ele) => !ele.includes("toolkits-app"));
+  dir.map((ele) => {
+    ele = ele.split("-");
+    ele.pop();
+    ele = ele.join("-");
+    const data = exportToC(
+      path + "/" + ele + "-app/pages/" + ele + "/index.mdx"
+    );
+    const jsonData = JSON.stringify(data, null, 2);
+    writeToFile("./data/" + ele + "-toc.json", jsonData);
+  });
+  const toolkitsPaths = [
+    path + "/toolkits-app/pages/hooks",
+    path + "/toolkits-app/pages/utils",
+  ];
+  toolkitsPaths.forEach((file) => {
+    const list = fs.readdirSync(file).filter((ele) => ele.includes("mdx"));
+    list.forEach((ele) => {
+      const data = exportToC(file + "/" + ele);
+      const jsonData = JSON.stringify(data, null, 2);
+      writeToFile("./data/" + ele.replace(".mdx", "") + "-toc.json", jsonData);
+    });
+  });
+};
+
 export default createTocJSON;
-createTocJSON();
