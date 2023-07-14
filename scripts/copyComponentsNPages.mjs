1dd51614acd602b66a84b1fc414e5a7e21c9722e
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-07-14 10:49:51 +0530

    Code optimisation, removed duplication, added JSDocs

--- scripts/copyComponentsNPages.mjs
+++ scripts/copyComponentsNPages.mjs
@@ -1,11 +1,47 @@
-// const fs = require("node:fs");
-// const ncp = require("ncp").ncp;
 import fs from "node:fs";
 import pkg from "ncp";
 const { ncp } = pkg;
 
 const devAppsPath = "./development";
 
+/**
+ * Filter function to exclude specific source files.
+ *
+ * @param {string} source - The source file path to be evaluated.
+ * @returns {boolean} - Returns false if the source file path matches any of the exclusion conditions, otherwise returns true.
+ */
+const filterFn = (source) => {
+  if (
+    source.includes("/_app.tsx") ||
+    source.includes("/_document.tsx") ||
+    source.includes("/index.tsx")
+  )
+    return false;
+  return true;
+};
+
+/**
+ * Copy file from source directory to target directory using filter function
+ *
+ * @param {string} source
+ * @param {string} target
+ * @returns {void}
+ */
+const copyToFolder = (source, target) => {
+  ncp(source, target, { filter: filterFn }, function (err) {
+    if (err) {
+      console.error(err);
+    }
+  });
+};
+
+/**
+ * Copy component and page folders to the documentation directory.
+ * This function retrieves the component and page folders from the development apps and copies them to the specified destination directories in the documentation.
+ * The function excludes specific folders and performs the copy operation using the copyToFolder() function.
+ *
+ * @returns {void}
+ */
 const getComponentFoldersList = () => {
   const componentList = fs
     .readdirSync(devAppsPath)
@@ -19,47 +55,7 @@ const getComponentFoldersList = () => {
     copyToFolder(sourcePath, "./docs/components")
   );
   pagesList.forEach((sourcePath) => copyToFolder(sourcePath, "./docs/pages"));
-  copyToolkits("./development/toolkits-app/pages", "./docs/pages");
+  copyToFolder("./development/toolkits-app/pages", "./docs/pages");
 };
 
-const copyToolkits = (source, target) => {
-  const filterFn = (source) => {
-    if (
-      source.includes("_app.tsx") ||
-      source.includes("_document.tsx") ||
-      source.includes("index.tsx")
-    )
-      return false;
-    return true;
-  };
-
-  ncp(source, target, { filter: filterFn }, function (err) {
-    if (err) {
-      console.error(err);
-    } else {
-      // console.log("Component Folder created successfully.");
-    }
-  });
-};
-const copyToFolder = (source, target) => {
-  const filterFn = (source) => {
-    if (
-      source.includes("_app.tsx") ||
-      source.includes("_document.tsx") ||
-      source.includes("index.tsx")
-    )
-      return false;
-    return true;
-  };
-
-  ncp(source, target, { filter: filterFn }, function (err) {
-    if (err) {
-      console.error(err);
-    } else {
-      // console.log("Component Folder created successfully.");
-    }
-  });
-};
-
-// module.exports = getComponentFoldersList;
 export default getComponentFoldersList;
