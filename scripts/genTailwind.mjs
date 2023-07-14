8757b333e80940d3d53db05522d5a3d84f8491c1
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-07-14 11:52:46 +0530

    genTailwind: Code cleaning, removed repetations, added JSDocs

--- scripts/genTailwind.mjs
+++ scripts/genTailwind.mjs
@@ -1,24 +1,15 @@
-// const fs = require("fs");
-const devAppsPath = "./development";
-// const _ = require("lodash");
 import _ from "lodash";
 import fs from "node:fs";
+import { readFile } from "fs/promises";
 
-const getTailWindFromDevApps = async () => {
-  const devappList = fs.readdirSync(devAppsPath);
-  const devAppsPaths = [
-    ...devappList.map((app) => devAppsPath + "/" + app + "/tailwind.config.js"),
-    "./docs/tailwind.config.js",
-  ];
+const devAppsPath = "./development";
 
-  const newTailwindObject = await readTailwindConfig(devAppsPaths);
-  writeToDocsTailwindConfig(
-    newTailwindObject.content,
-    newTailwindObject.theme,
-    newTailwindObject.plugins
-  );
-};
-
+/**
+ * Retrieve Tailwind configuration from development apps and generate a new Tailwind configuration file.
+ * This function reads the Tailwind configuration files from the specified development apps and additional documentation config file. It merges the content, theme, and plugins data from these files to generate a new Tailwind configuration object. The generated configuration object is then written to the documentation Tailwind config file.
+ *
+ * @returns {Promise<void>} - A promise that resolves once the new Tailwind configuration file is written.
+ */
 const writeToDocsTailwindConfig = (contentArray, themeObject, pluginsArray) => {
   try {
     const replacementContent =
@@ -34,10 +25,16 @@ const writeToDocsTailwindConfig = (contentArray, themeObject, pluginsArray) => {
       .replace(/plugins:\s*\[(.*?)\]/s, `${replacementPlugins}`);
     fs.writeFileSync(tailwindConfigPath, updatedConfigFile, "utf8");
   } catch (error) {
-    console.log("error>>", error);
+    console.log("Error while writing to tailwind Config>>", error);
   }
 };
 
+/**
+ * Read the Tailwind configuration from the specified paths and generate a Tailwind object.
+ * This function reads the Tailwind configuration files from the given paths and extracts the content, plugins, and theme information. It returns a generated Tailwind object containing the extracted data.
+ * @param {string[]} paths - An array of paths to the Tailwind configuration files.
+ * @returns {Promise<Object>} - A promise that resolves to the generated Tailwind object.
+ */
 const readTailwindConfig = async (paths) => {
   const contentRegex = /content:\s*\[(.*?)\]/s;
   const themeRegex = /theme:\s*(.*?)plugins/s;
@@ -69,75 +66,98 @@ const readTailwindConfig = async (paths) => {
   return generatedTailwindObject;
 };
 
-const contentFetcher = (path, regex) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, "utf8", (err, data) => {
-      if (err) {
-        console.log("Error in Reading tailwind file!!!");
-        reject(err);
-        return;
-      }
-      try {
-        const contentResult = data.match(regex);
-        let array = contentResult[1]
-          .split(",\n")
-          .map((val) => val.replaceAll("\n", "").replaceAll(" ", ""))
-          .filter(Boolean);
-        resolve(array);
-      } catch (error) {
-        reject(error);
-      }
-    });
-  });
+/**
+ * Fetch content from a file based on a regular expression pattern.
+ * This function reads the content of the specified file using the readFile function, matches it against the provided regular expression pattern, and returns the captured content.
+ *
+ * @param {string} path - The path of the file to read.
+ * @param {RegExp} regex - The regular expression pattern to match against the file content.
+ * @returns {Promise<string>} - A promise that resolves to the captured content.
+ */
+const fetcher = async (path, regex) => {
+  const data = await readFile(path, "utf8");
+  const contentResults = data.match(regex);
+  return contentResults[1];
 };
 
-const themeFetcher = (path, regex) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, "utf8", (err, data) => {
-      if (err) {
-        console.log("Error in Reading tailwind file!!!");
-        reject(err);
-        return;
-      }
-      try {
-        const contentResult = data.match(regex);
-        let array = contentResult[1]
-          .replaceAll("},\n", "}")
-          .replaceAll("\n", "")
-          .replaceAll(/\s{2,}/g, "")
-          .replaceAll(",}", "}")
-          .replace(/,\s*$/, "");
-        const validString = array.replace(
-          /(['"])?([a-zA-Z0-9_-]+)(['"])?:/g,
-          '"$2": '
-        );
-        resolve(validString);
-      } catch (error) {
-        reject(error);
-      }
-    });
-  });
+/**
+ * Fetch and process content from a file based on a regular expression pattern.
+ * This function reads the content of the specified file using the fetcher function and processes it based on the provided regular expression pattern. It splits the content by comma and newline, removes extra whitespace and empty values, and returns the resulting array of processed content.
+ *
+ * @param {string} path - The path of the file to read.
+ * @param {RegExp} regex - The regular expression pattern to match against the file content.
+ * @returns {Promise<string[]>} - A promise that resolves to an array of processed content.
+ * @throws {Error} - If there is an error in reading the file or processing the content.
+ */
+const contentFetcher = async (path, regex) => {
+  try {
+    const array = (await fetcher(path, regex))
+      .split(",\n")
+      .map((val) => val.replaceAll("\n", "").replaceAll(" ", ""))
+      .filter(Boolean);
+    return array;
+  } catch (error) {
+    console.log("Error in reading tailwind file:", error);
+    throw error;
+  }
 };
 
-const pluginsFetcher = (path, regex) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, "utf8", (err, data) => {
-      if (err) {
-        console.log("Error in Reading tailwind file!!!");
-        reject(err);
-        return;
-      }
-      try {
-        const pluginResults = data.match(regex);
-        let array = pluginResults[1]
-          .split(",")
-          .map((val) => val.replaceAll("\n", "").replaceAll(" ", ""));
-        resolve(array);
-      } catch (error) {
-        reject(error);
-      }
-    });
-  });
+/**
+ * Fetch and process theme data from a file based on a regular expression pattern.
+ * This function reads the content of the specified file using the fetcher function and processes the theme data based on the provided regular expression pattern. It performs string replacements and formatting to ensure a valid JSON string representing the theme data.
+ *
+ * @param {string} path - The path of the file to read.
+ * @param {RegExp} regex - The regular expression pattern to match against the file content.
+ * @returns {Promise<string>} - A promise that resolves to a valid JSON string representing the processed theme data.
+ */
+const themeFetcher = async (path, regex) => {
+  const array = (await fetcher(path, regex))
+    .replaceAll("},\n", "}")
+    .replaceAll("\n", "")
+    .replaceAll(/\s{2,}/g, "")
+    .replaceAll(",}", "}")
+    .replace(/,\s*$/, "");
+  const validString = array.replace(
+    /(['"])?([a-zA-Z0-9_-]+)(['"])?:/g,
+    '"$2": '
+  );
+  return validString;
+};
+
+/**
+ * Fetch and process plugin data from a file based on a regular expression pattern.
+ * This function reads the content of the specified file using the fetcher function and processes the plugin data based on the provided regular expression pattern. It splits the data by comma, removes newline characters and extra whitespace, and returns the resulting array of processed plugin data.
+ *
+ * @param {string} path - The path of the file to read.
+ * @param {RegExp} regex - The regular expression pattern to match against the file content.
+ * @returns {Promise<string[]>} - A promise that resolves to an array of processed plugin data.
+ */
+const pluginsFetcher = async (path, regex) => {
+  let array = (await fetcher(path, regex))
+    .split(",")
+    .map((val) => val.replaceAll("\n", "").replaceAll(" ", ""));
+  return array;
+};
+
+/**
+ * Retrieve Tailwind configuration from development apps and generate a new Tailwind configuration file.
+ * This function reads the Tailwind configuration files from the specified development apps and additional documentation config file. It merges the content, theme, and plugins data from these files to generate a new Tailwind configuration object. The generated configuration object is then written to the documentation Tailwind config file.
+ *
+ * @returns {Promise<void>} - A promise that resolves once the new Tailwind configuration file is written.
+ */
+const getTailWindFromDevApps = async () => {
+  const devappList = fs.readdirSync(devAppsPath);
+  const devAppsPaths = [
+    ...devappList.map((app) => devAppsPath + "/" + app + "/tailwind.config.js"),
+    "./docs/tailwind.config.js",
+  ];
+
+  const newTailwindObject = await readTailwindConfig(devAppsPaths);
+  writeToDocsTailwindConfig(
+    newTailwindObject.content,
+    newTailwindObject.theme,
+    newTailwindObject.plugins
+  );
 };
 
 export default getTailWindFromDevApps;
