52dfdf6e0e9071ee81d70d6e6fc509736c69db76
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-07-18 16:07:38 +0530

    Fixed lit errors for toolkit

--- toolkit/hooks/src/use-debounce-effect.ts
+++ toolkit/hooks/src/use-debounce-effect.ts
@@ -8,15 +8,15 @@
 import React from "react";
 import { useIsoMorphicEffect } from "./use-iso-morphic-effect";
 
-export let useDebounceEffect = (
-  callback: React.EffectCallback,
-  delay: number,
-  deps: React.DependencyList
+export const useDebounceEffect = (
+	callback: React.EffectCallback,
+	delay: number,
+	deps: React.DependencyList,
 ) => {
-  useIsoMorphicEffect(() => {
-    const debounceTimer = setTimeout(() => {
-      callback();
-    }, delay);
-    return () => clearTimeout(debounceTimer);
-  }, deps);
+	useIsoMorphicEffect(() => {
+		const debounceTimer = setTimeout(() => {
+			callback();
+		}, delay);
+		return () => clearTimeout(debounceTimer);
+	}, deps);
 };
--- toolkit/hooks/src/use-did-mount-effect.ts
+++ toolkit/hooks/src/use-did-mount-effect.ts
@@ -9,11 +9,11 @@ import React from "react";
 
 // https://stackoverflow.com/a/57941438/17167844
 
-export let useDidMountEffect = (func: any, deps: any) => {
-  const didMount = React.useRef(false);
+export const useDidMountEffect = (func: any, deps: any) => {
+	const didMount = React.useRef(false);
 
-  React.useEffect(() => {
-    if (didMount.current) func();
-    else didMount.current = true;
-  }, deps);
+	React.useEffect(() => {
+		if (didMount.current) func();
+		else didMount.current = true;
+	}, deps);
 };
