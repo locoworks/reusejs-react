7c1b4a13a70ee8f8f9f36497b2219a638e0d7000
Md Amir Sohail <betalectic@Betalectics-MacBook-Air.local>
2023-07-19 12:40:22 +0530

    created data table project

---
+++ development/data-table-app/components/data-table/Example/Example.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// Import component New Componnet created here
+// import <component-name> from "@locoworks/remark-folder-<component-name>"
+
+const Example = () => {
+  return (
+    <div className="flex flex-col items-center gap-x-3 justify-center py-10 mt-10 border rounded bg-gray-50">
+      {/*Example Code Goes here */}
+    </div>
+  );
+};
+
+export default Example;
