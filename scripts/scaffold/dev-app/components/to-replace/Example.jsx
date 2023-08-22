d2599318d69f25aa7abce6976c1def623a324856
upneet-bit <hunjanriya@gmail.com>
2023-08-22 17:08:35 +0530

    restored the file : Added Example Component Scaffolding to dev-app

---
+++ scripts/scaffold/dev-app/components/to-replace/Example.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// Import component New Componnet created here
+// import <component-name> from "@locoworks/remark-folder-<component-name>"
+
+const Example = () => {
+  return (
+    <div className="flex flex-col items-center justify-center py-10 mt-10 border rounded gap-x-3 bg-gray-50">
+      {/*Example Code Goes here */}
+    </div>
+  );
+};
+
+export default Example;
