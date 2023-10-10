d99a151bed43957df8bec16a5122551509121a92
upneet-betalectic <upneet@betalectic.com>
2023-10-10 16:46:01 +0530

    implementing forwardRef

--- development/video-recorder-app/components/video-recorder/Example/Example.tsx
+++ development/video-recorder-app/components/video-recorder/Example/Example.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HeadessVideoRecorder } from "@locoworks/reusejs-react-video-recorder";
+import { HeadlessVideoRecorder } from "@locoworks/reusejs-react-video-recorder";
 
 // Import New Component created here
 // import <component-name> from "@locoworks/remark-folder-<component-name>"
@@ -7,7 +7,7 @@ import { HeadessVideoRecorder } from "@locoworks/reusejs-react-video-recorder";
 const Example = () => {
 	return (
 		<div className="flex flex-col items-center justify-center py-10 mt-10 border rounded gap-x-3 bg-gray-50">
-			<HeadessVideoRecorder />
+			<HeadlessVideoRecorder />
 		</div>
 	);
 };
--- development/video-recorder-app/components/video-recorder/autoStop/autoStop.tsx
+++ development/video-recorder-app/components/video-recorder/autoStop/autoStop.tsx
@@ -1,13 +1,73 @@
-import React from "react";
-import { HeadessVideoRecorder } from "@locoworks/reusejs-react-video-recorder";
+import React, { useEffect, useRef } from "react";
+import { HeadlessVideoRecorder } from "@locoworks/reusejs-react-video-recorder";
 
 // Import New Component created here
 // import <component-name> from "@locoworks/remark-folder-<component-name>"
+interface HeadlessVideoRecorderRef {
+	recording: string;
+	handleDownload: () => void;
+	showPreview: () => void;
+}
+const AutoStopVideoRecorder: React.FC = () => {
+	const videoRecorderRef = useRef<HeadlessVideoRecorderRef>(null);
+	const [isLoading, setIsLoading] = React.useState(true);
 
-const AutoStopVideoRecorder = () => {
+	useEffect(() => {
+		console.log(
+			"Recording in PARENT changed",
+			videoRecorderRef.current?.recording,
+		);
+		console.log(videoRecorderRef.current, "___obj__");
+
+		if (videoRecorderRef.current?.recording === "inactive") {
+			setIsLoading(false);
+		}
+	}, [videoRecorderRef.current?.recording]);
+
+	const downloadHandler = () => {
+		if (videoRecorderRef.current) {
+			videoRecorderRef.current.handleDownload();
+		} else {
+			console.error("videoRecorderRef is null");
+		}
+	};
+
+	const showPreview = () => {
+		if (videoRecorderRef.current) {
+			videoRecorderRef.current.showPreview();
+		} else {
+			console.error("videoRecorderRef is null");
+		}
+	};
+
 	return (
 		<div className="flex flex-col items-center justify-center py-10 mt-10 border rounded gap-x-3 bg-gray-50">
-			<HeadessVideoRecorder autoStop={true} timeInMs={10000} />
+			<HeadlessVideoRecorder
+				ref={videoRecorderRef}
+				autoStop={true}
+				timeInMs={10000}
+				handleDownloadCallback={(setRecording) => {
+					console.log(setRecording);
+					setRecording("preview");
+				}}
+			/>
+			{!isLoading && videoRecorderRef.current && (
+				<>
+					{videoRecorderRef.current.recording === "inactive" && (
+						<button className="p-2 bg-blue-300 border" onClick={showPreview}>
+							Show Preview
+						</button>
+					)}
+					{videoRecorderRef.current.recording === "recorded" && (
+						<button
+							className="p-2 bg-blue-300 border"
+							onClick={downloadHandler}
+						>
+							Download Video
+						</button>
+					)}
+				</>
+			)}
 		</div>
 	);
 };
