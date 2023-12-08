d8afb0c5b5122c3a59e24e7e42bc8ee50c9e9b3f
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-12-08 12:31:20 +0530

    cd: Build issue file renaming

--- components/file-upload/index.ts
+++ components/file-upload/index.ts
@@ -1,2 +1,2 @@
-export { default as HeadlessFileUpload } from "./src/HeadlessFileUpload";
 export { default as ReuseFileUpload } from "./src/ReuseFileUpload";
+export { default as HeadlessFileUpload } from "./src/HeadlessFileUpload";
