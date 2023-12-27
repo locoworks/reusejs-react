75cbac86112651d69573694353ce0561b371f418
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-12-27 22:20:04 +0530

    Feat: Added support for Empty date value

--- development/date-picker-app/components/date-picker/Example/Example.tsx
+++ development/date-picker-app/components/date-picker/Example/Example.tsx
@@ -11,15 +11,26 @@ const Example: React.FC = () => {
 
 	const printer = (date: Date) => {
 		console.log("Date", date);
+		setSelectedDate(date);
 	};
+	const [selectedDate, setSelectedDate] = React.useState<any>("");
+
 	return (
-		<div className="flex flex-col items-center justify-center py-10 mt-10 border rounded gap-x-3 bg-gray-50">
+		<div className="flex flex-col gap-y-2 items-center justify-center py-10 mt-10 border rounded gap-x-3 bg-gray-50">
 			<HeadlessDatePicker
 				errorText="Error in Date Format!!"
 				invalidDateClasses="outline outline-red-500"
 				calendarBaseClasses={calendarClasses}
 				dateCallback={printer}
 			/>
+			<button
+				className="bg-blue-600 px-4 py-1 rounded-xl text-white font-bold hover:bg-blue-700"
+				onClick={() => {
+					console.log("SELECTED DATE IS>>>", selectedDate);
+				}}
+			>
+				Show Selected Date
+			</button>
 		</div>
 	);
 };
